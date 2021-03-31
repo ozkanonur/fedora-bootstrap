baaa52211d678ab1dfe79623d8f10fa88c7df224
Onur <onurozkan.dev@outlook.com>
2021-03-31 11:39:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-# tools
+# fish
 sudo dnf -y install fish
+curl -L https://get.oh-my.fish | fish
+omf install eden
+
+# x11 headers
 sudo dnf -y install libX11-devel
+
+# vim
 sudo dnf -y install vim
-sudo dnf -y install htop
 
 # docker
 sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
@@ -15,3 +20,6 @@ sudo systemctl enable docker
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 sudo systemctl restart docker
+
+# other tools
+sudo dnf -y install htop
