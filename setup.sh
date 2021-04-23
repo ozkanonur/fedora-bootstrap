c509b0979e7c77e0e3c0db6b5c03d516cd8dc86d
Onur <onurozkan.dev@outlook.com>
2021-04-23 07:14:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,34 +1,27 @@
 #!/bin/sh
 
-# fish
-sudo dnf -y install fish
-curl -L https://get.oh-my.fish | fish
-omf install eden
+# browsers
+dnf -y install chromium
 
-# x headers
-sudo dnf -y install libX11-devel libXft-devel libXinerama-devel
-
-sudo dnf -y install @base-x xterm
+# x11
+dnf -y install libX11-devel libXft-devel libXinerama-devel
 
 # file manager
-git clone https://github.com/ranger/ranger .ranger
+git clone https://github.com/ranger/ranger $HOME/.ranger
 cd .ranger && sudo make install && cd ..
 
-# vim
-sudo dnf -y install vim
+# text editors
+dnf -y install vim leafpad
+
+# tools
+dnf -y install brightnessctl rofi xrandr feh htop tlp
 
 # docker
-sudo grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
-sudo firewall-cmd --permanent --zone=trusted --add-interface=docker0
-sudo firewall-cmd --permanent --zone=FedoraWorkstation --add-masquerade
-sudo dnf install moby-engine docker-compose
-sudo systemctl enable docker
-sudo groupadd docker
-sudo gpasswd -a $USER docker
-sudo systemctl restart docker
-
-# wm environment
-sudo dnf -y install rofi feh fontawesome-fonts
-
-# other tools
-sudo dnf -y install htop tlp-rdw leafpad NetworkManager
+grubby --update-kernel=ALL --args="systemd.unified_cgroup_hierarchy=0"
+firewall-cmd --permanent --zone=trusted --add-interface=docker0
+firewall-cmd --permanent --zone=FedoraWorkstation --add-masquerade
+dnf install moby-engine docker-compose
+systemctl enable docker
+groupadd docker
+gpasswd -a $USER docker
+systemctl restart docker
