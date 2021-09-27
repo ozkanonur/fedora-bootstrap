b08a4eb70a7608f5805fe2c773ce879a31532ea0
Onur <onurozkan.dev@outlook.com>
2021-09-27 18:49:47 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ dnf -y install zsh
 dnf -y install libX11-devel libXft-devel libXinerama-devel libXrandr-devel libXi-devel libX11-xcb libXext-devel
 
 # necessary tools
-dnf -y install gcc git xrandr xcompmgr xset xcompmgr imlib2-devel NetworkManager-tui tlp tlp-rdw \
+dnf -y install gcc git xrandr xset imlib2-devel NetworkManager-tui tlp tlp-rdw \
 	pulseaudio-utils ripgrep brightnessctl valgrind freerdp flameshot feh setxkbmap \
 	openvpn dbus-devel libconfig-devel libdrm-devel libev-devel libxcb-devel mesa-libGL-devel \
 	meson pcre-devel pixman-devel uthash-devel xcb-util-image-devel xcb-util-renderutil-devel xorg-x11-proto-devel
